e4eb3cdabf02931e527dcf8a74fc63d3fd59e15c
hafidzyami <18221074@std.stei.itb.ac.id>
2024-05-27 17:56:39 +0700

    fix bug

--- app/(customer)/(UMKM)/ListMenu.tsx
+++ app/(customer)/(UMKM)/ListMenu.tsx
@@ -32,7 +32,9 @@ const UMKMenu = () => {
   const [refreshing, setRefreshing] = useState<boolean>(false);
   let arrayCartCheckout: cartCheckout[] = [];
 
+
   const fetchData = async () => {
+    console.log("ngefetch data")
     try {
       const docRef = doc(
         getFirestore(),
@@ -42,7 +44,9 @@ const UMKMenu = () => {
       const docSnap = await getDoc(docRef);
       if (docSnap.exists()) {
         setMenu(docSnap.data().menus);
+        console.log(docSnap.data().menus)
       } else {
+        console.log("gaada")
         setMenu([]);
       }
     } catch (error) {}
@@ -162,6 +166,7 @@ const UMKMenu = () => {
   );
   return (
     <View style={styles.container}>
+      <Text>{params.id}</Text>
       {loading ? (
         <ActivityIndicator size="large" color="#F8E800" />
       ) : (
