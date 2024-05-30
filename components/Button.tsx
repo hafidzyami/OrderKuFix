94c2e8c8a75e73e879e5deef0517a2110d71f4e7
muhdastinfauzi <muhammaddastinfauzi@gmail.com>
2024-05-30 18:37:13 +0700

    update

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,39 +1,46 @@
-import React from 'react';
-import { Text, StyleSheet, Pressable, ColorValue } from 'react-native';
+import React from "react";
+import { Text, StyleSheet, Pressable, ColorValue } from "react-native";
 
 interface ButtonProps {
   onPress: () => void;
   title: string;
   color: ColorValue;
   opacity?: number;
-  borderColor? : any;
+  borderColor?: any;
 }
 
-export function ButtonCustom({ onPress, title, color, opacity, borderColor }: ButtonProps) {
+export function ButtonCustom({
+  onPress,
+  title,
+  color,
+  opacity,
+  borderColor,
+}: ButtonProps) {
   const styles = StyleSheet.create({
     button: {
-      alignItems: 'center',
-      justifyContent: 'center',
+      alignItems: "center",
+      justifyContent: "center",
       paddingVertical: 12,
       paddingHorizontal: 32,
-      borderRadius: 4,
+      borderRadius: 100,
       elevation: 3,
       backgroundColor: color,
       opacity: opacity ?? 1,
-      borderColor : borderColor // default opacity is 1 if not provided
+      borderColor: borderColor, // default opacity is 1 if not provided
+      color: "black",
     },
     text: {
       fontSize: 16,
       lineHeight: 21,
-      fontWeight: 'bold',
+      fontWeight: "bold",
       letterSpacing: 0.25,
-      color: 'black',
-      opacity : 1
+      color: "black",
+      opacity: 1,
     },
   });
 
   return (
-    <Pressable style={styles.button} onPress={onPress}>
+    <Pressable  style={styles.button} onPress={onPress}>
       <Text style={styles.text}>{title}</Text>
     </Pressable>
   );
